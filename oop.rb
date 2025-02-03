ad1581f949a1ab7ecfdfd588bffdce01c8b86497
Leon Shimizu <lmshimizu@gmail.com>
2025-02-03 18:24:30 +1000

    OOP Introduction

---
+++ oop.rb
@@ -0,0 +1,37 @@
+# encapsulation
+# bundle data and methods that operate on that data within one class
+
+# abstraction
+# hide complex implementation/logic details and only show what is necessary
+
+# name = "Leon"
+# puts name.class
+# number = 10
+# puts number.class
+
+# class Car
+#   # methods and logic
+# end
+
+# my_car = Car.new
+# puts my_car.class
+
+
+# attr_reader :name creates a getter method for @name.
+# attr_writer :name creates a setter method for @name.
+# attr_accessor :name creates both getter and setter methods.
+
+
+
+# class Person
+#   attr_accessor :name, :age
+
+#   def initialize(name, age)
+#     @name = name
+#     @age = age
+#   end
+# end
+
+# person = Person.new("Alanna", 23)
+# puts person.name
+# puts person.age
