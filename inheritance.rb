cce0cbfec3f57c7b07869b0c7f16f8aca5e432e2
Leon Shimizu <lmshimizu@gmail.com>
2025-02-04 19:28:00 +1000

    Polymorphism and method overwriting exercises

--- inheritance.rb
+++ inheritance.rb
@@ -33,53 +33,78 @@
 
 
 
-# Task: Create a Person superclass and Student and Teacher subclasses.
+# Task: Modify the introduce method in Student and Teacher to include more details, ensuring the use of super.
 # Instructions:
-# Create a new Ruby file named inheritance.rb.
-# Define a Person class with attributes name and age, and a method introduce.
-# Create Student and Teacher classes that inherit from Person.
-# Add a grade attribute to Student and a subject attribute to Teacher.
-# Instantiate objects from both subclasses and call their methods.
+# In your Student class, add an attribute school and include it in the introduce method using super.
+# In your Teacher class, add an attribute years_of_experience and include it in the introduce method using super.
+# Test your code by creating new instances and calling introduce.
 
-# super 
+class Person
+  def initialize(name, age)
+    @name = name
+    @age = age
+  end
 
-# class Person
-#   def initialize(name, age)
-#     @name = name
-#     @age = age
-#   end
+  def introduce
+    "Hi, my name is #{@name} and I am #{@age} years old."
+  end
+end
 
-#   def introduce
-#     "Hi, my name is #{@name} and I am #{@age} years old."
+class Student < Person
+  def initialize(name, age, grade, school)
+    super(name, age)
+    @grade = grade
+    @school = school
+  end
+
+  def introduce
+    super + " I'm in grade #{@grade} and I go to #{@school}."
+  end
+end
+
+class Teacher < Person
+  def initialize(name, age, subject, years_of_experience)
+    super(name, age)
+    @subject = subject
+    @years_of_experience = years_of_experience
+  end
+
+  def introduce
+    "#{super} I teach #{@subject}. And I've been teaching for #{@years_of_experience} years."
+  end
+end
+
+student = Student.new("Leon", 25, "12", "FD")
+puts student.introduce
+
+teacher = Teacher.new("Charles", 26, "Computer Science", 50)
+puts teacher.introduce
+
+
+
+
+# class Shape
+#   def draw
+#     "Drawing a shape..."
 #   end
 # end
 
-# class Student < Person
-#   def initialize(name, age, grade)
-#     super(name, age)
-#     @grade = grade
-#   end
-
-#   def introduce
-#     super + " I'm in grade #{@grade}."
+# class Circle < Shape
+#   def draw
+#     "Drawing a circle!"
 #   end
 # end
 
-# class Teacher < Person
-#   def initialize(name, age, subject)
-#     super(name, age)
-#     @subject = subject
-#   end
-
-#   def introduce
-#     "#{super} I teach #{@subject}."
+# class Square < Shape
+#   def draw
+#     "Drawing a square!"
 #   end
 # end
 
-# student = Student.new("Leon", 25, "12")
-# puts student.introduce
+# shapes = [Circle.new, Square.new, Shape.new]
 
-# teacher = Teacher.new("Charles", 26, "Computer Science")
-# puts teacher.introduce
+# shapes.each do |shape|
+#   puts shape.draw
+# end
 
 
